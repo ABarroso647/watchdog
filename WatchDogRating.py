4ab3f6e72006be9aaebdca6507de619a9ffcc460
Roton647 <abarroso@edu.uwaterloo.ca>
2020-08-22 03:38:31 -0400

    added test flask files <3

---
+++ WatchDogRating.py
@@ -0,0 +1,26 @@
+#from google.cloud import storage
+from flask import Flask, jsonify
+from flask import request
+'''
+    def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_blob_name = "storage-object-name"
+    # destination_file_name = "local/path/to/file"
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        "Blob {} downloaded to {}.".format(
+            source_blob_name, destination_file_name
+        )
+    )
+'''
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
