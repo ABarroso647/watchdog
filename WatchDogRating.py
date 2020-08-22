69df9a3529383d60ef6e8ff97e97a93514ed83a0
Roton647 <abarroso@edu.uwaterloo.ca>
2020-08-22 06:27:32 -0400

    new file update

--- WatchDogRating.py
+++ WatchDogRating.py
@@ -19,8 +19,9 @@ from flask import request
     )
 '''
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['POST'])
 def hello_world():
-    return "Hello World!"
+    data = request.data
+    return data
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
